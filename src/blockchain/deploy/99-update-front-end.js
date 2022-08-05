676d703314ff811345f801fa2e450538550aeeda
anushgs <anush443@gmail.com>
2022-08-05 19:37:44 +0530

    minor

---
+++ src/blockchain/deploy/99-update-front-end.js
@@ -0,0 +1,5 @@
+const fs = require("fs")
+const { network } = require("../../constants/cAddresses.json")
+
+const frontEndContractsFile = "../../constants/contractAddresses.json"
+const frontEndAbiFile = "../../constants/abi.json"
